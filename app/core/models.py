5e59c5ddade08f2e5338991ee2da28921b82122d
kratos2404 <9024763630s@gmail.com>
2022-11-10 16:23:52 +0530

    solve error

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('User must have an email address.')
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using = self._db)
         return user
@@ -21,13 +21,14 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
+        return user
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
 
     objects = UserManager()
 
-    USERNAME_FIELD='email'
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@
 Teat custome django managment command
 """
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -22,10 +22,10 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """test waiting for database when getting OperationalError."""
-        patched_check.side_effort = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 1)
+        self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
